a9207bacf237ccb31775c09b08467f5961235796
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2024-01-20 11:54:33 +0100

    Update a user

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,23 @@
+import prisma from "@/lib/db/prisma";
+import { User } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+type UpdateUserReqBody = Partial<Omit<User, "id">>;
+
+export const PATCH = async (req: NextRequest, route: { params: { id: string }}) => {
+  const body: UpdateUserReqBody = await req.json();
+  const { id: userId } = route.params;
+
+  try {
+    const user = await prisma.user.update({ 
+      where: { id: parseInt(userId) }, 
+      data: { ...body }
+    });
+
+    return NextResponse.json({ user });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json("Something went wrong updating the user", { status: 500 });
+  }
+}
